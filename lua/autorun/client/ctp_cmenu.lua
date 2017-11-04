133c65882d1d0fbeab8899c062b6f9f8f3b1e3b4
Tenrys <tenrys.iaido@live.fr>
2017-11-04 13:16:15 +0100

    use callback functionality

--- lua/autorun/client/ctp_cmenu.lua
+++ lua/autorun/client/ctp_cmenu.lua
@@ -15,29 +15,31 @@ hook.Add("HUDPaint", "ctp_cmenu", function()
 	local printed = false
 	local w = Color(194, 210, 225)
 	local g = Color(127, 255, 127)
-	list.Set(
-		"DesktopWindows",
-		"ZCTP",
-		{
-			title = "Thirdperson",
-			icon = WebMaterial("icon64_ctp", "https://gmlounge.us/fastdl/cmenu_zoom_extend.png"),
-			width = 960,
-			height = 700,
-			onewindow = true,
-			init = function(icn, pnl)
-				pnl:Remove()
-				if not printed then
-					chat.AddText(w, "Go in the ", g, "Spawn Menu", w, " > ", g, "Utilities", w, " > ", g, "CTP", w, " category to customize the third person!")
-					printed = true
+	local mat = WebMaterial("icon64_ctp", "https://gmlounge.us/fastdl/cmenu_zoom_extend.png", function(name, url, mat)
+		list.Set(
+			"DesktopWindows",
+			"ZCTP",
+			{
+				title = "Thirdperson",
+				icon = mat,
+				width = 960,
+				height = 700,
+				onewindow = true,
+				init = function(icn, pnl)
+					pnl:Remove()
+					if not printed then
+						chat.AddText(w, "Go in the ", g, "Spawn Menu", w, " > ", g, "Utilities", w, " > ", g, "CTP", w, " category to customize the third person!")
+						printed = true
+					end
+					RunConsoleCommand("ctp")
+
+					return false
 				end
-				RunConsoleCommand("ctp")
+			}
+		)
 
-				return false
-			end
-		}
-	)
-
-	CreateContextMenu()
+		CreateContextMenu()
+	end)
 
 	hook.Remove("HUDPaint", "ctp_cmenu")
 end)
