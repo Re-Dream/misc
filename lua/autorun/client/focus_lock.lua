5f51a8aa57cb22ad9b0fea63e628427fdf898374
Tenrys <tenrys.iaido@live.fr>
2018-02-16 05:23:05 +0100

    asdsf

--- lua/autorun/client/focus_lock.lua
+++ lua/autorun/client/focus_lock.lua
@@ -1,15 +1,6 @@
 
 local tag = "focus_lock"
 
-local windowed = system.IsWindowed()
-local firstInput = false
-local function HasFocus()
-	if windowed and not firstInput then
-		return false
-	else
-		return system.HasFocus()
-	end
-end
 local function RemovePanel()
 	focus_panel:Remove()
 end
@@ -21,23 +12,18 @@ local function CreatePanel()
 end
 
 hook.Add("Think", tag, function()
-	if not HasFocus() and not IsValid(focus_panel) then
+	if not system.HasFocus() and not IsValid(focus_panel) then
 		if GetConVarNumber("developer") == 1 then
 			print("Lost focus")
 		end
 		hook.Run("OnFocusChanged", false)
 		CreatePanel()
-	elseif HasFocus() and IsValid(focus_panel) then
+	elseif system.HasFocus() and IsValid(focus_panel) then
 		if GetConVarNumber("developer") == 1 then
 			print("Gained focus")
 		end
 		hook.Run("OnFocusChanged", true)
 		RemovePanel()
 	end
-end)
-
-hook.Add("PlayerButtonDown", tag, function()
-	firstInput = true
-	hook.Remove("PlayerButtonDown", tag)
 end)
 
