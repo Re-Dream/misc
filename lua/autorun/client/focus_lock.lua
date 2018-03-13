1a456f700874a5a2c86c52e11b436daff801b35e
Tenrys <tenrys.iaido@live.fr>
2018-03-13 12:52:05 +0100

    add cvar for focus_lock in case of issues (old, just didnt push)

--- lua/autorun/client/focus_lock.lua
+++ lua/autorun/client/focus_lock.lua
@@ -6,12 +6,16 @@ local function RemovePanel()
 end
 local function CreatePanel()
 	focus_panel = vgui.Create("EditablePanel")
-	focus_panel:SetKeyboardInputEnabled(false)
+	focus_panel:SetKeyboardInputEnabled(true)
 	focus_panel:SetVisible(true)
 	focus_panel:MakePopup()
 end
 
+local cvar = CreateClientConVar(tag, "1")
+
 hook.Add("Think", tag, function()
+	if not cvar:GetBool() then return end
+
 	if not system.HasFocus() and not IsValid(focus_panel) then
 		if GetConVarNumber("developer") == 1 then
 			print("Lost focus")
