45c6283799ed3e93d540937804782d3444fe7fe0
Marceau Maubert <tenrys.iaido@live.fr>
2018-04-05 13:58:11 +0200

    revert the commit that broke this lol

--- lua/autorun/client/focus_lock.lua
+++ lua/autorun/client/focus_lock.lua
@@ -1,6 +1,15 @@
 
 local tag = "focus_lock"
 
+local windowed = system.IsWindowed()
+local firstInput = false
+local function HasFocus()
+	if windowed and not firstInput then
+		return false
+	else
+		return system.HasFocus()
+	end
+end
 local function RemovePanel()
 	focus_panel:Remove()
 end
@@ -14,20 +23,23 @@ end
 local cvar = CreateClientConVar(tag, "1")
 
 hook.Add("Think", tag, function()
-	if not cvar:GetBool() then return end
-
-	if not system.HasFocus() and not IsValid(focus_panel) then
+	if not HasFocus() and not IsValid(focus_panel) then
 		if GetConVarNumber("developer") == 1 then
 			print("Lost focus")
 		end
 		hook.Run("OnFocusChanged", false)
 		CreatePanel()
-	elseif system.HasFocus() and IsValid(focus_panel) then
+	elseif HasFocus() and IsValid(focus_panel) then
 		if GetConVarNumber("developer") == 1 then
 			print("Gained focus")
 		end
 		hook.Run("OnFocusChanged", true)
 		RemovePanel()
 	end
+end)
+
+hook.Add("PlayerButtonDown", tag, function()
+	firstInput = true
+	hook.Remove("PlayerButtonDown", tag)
 end)
 
